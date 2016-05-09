6afb7d117abeb3d4853322ab892f63b45a2bfe0d
Tracy Wu <sytracywu@gmail.com>
2016-05-09 14:55:29 +0100

    Attempting to add in Googlemap

---
+++ public/js/map.js
@@ -0,0 +1,15 @@
+var MapApp = MapApp || {};
+
+MapApp.initialize = function(){
+  this.canvas = document.getElementById('map-canvas');
+
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(51.506178,-0.088369),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  this.map = new google.maps.Map(this.canvas, mapOptions);
+};
+
+MapApp.initialize();
