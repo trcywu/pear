8458ae43ce82042ce6ef1a248a28e70f8dba79e5
CJAllen056 <christopherjallen056@gmail.com>
2016-05-11 18:17:31 +0100

    changed api key

--- public/js/map.js
+++ public/js/map.js
@@ -131,7 +131,7 @@ Pear.getVenues = function(lat, lng){
   $.each(Pear.venueTypes, function(i, venueType) {
     return $.ajax({
       type: "GET",
-      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lng+"&radius=500&type="+venueType+"&key=AIzaSyCg9HSSgl7ERpRyl2AxSHZgrwAUoqXWUno"
+      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lng+"&radius=500&type="+venueType+"&key=AIzaSyAtQYsLy07B5CVO2lZmmM4a8KKchfnUTdg"
     }).done(self.loopThroughVenues)
   })
 
