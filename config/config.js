c3cd8702bc2e7ca6b732137fe761e95393f325ef
chansecampbell <chansecampbell@gmail.com>
2016-05-13 12:15:03 +0100

    Hotfix for port

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	port: process.env.port || 3000,
+	port: process.env.PORT || 3000,
 	database: process.env.MONGODB_URI || "mongodb://localhost/wdi-project-3",
 	secret: process.env.SECRET || "ADs&*hel02#£!!jsbbX"
 }
