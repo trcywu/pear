3bd555c849d0fc21243601547560f985f088f20a
CJAllen056 <christopherjallen056@gmail.com>
2016-05-13 11:21:25 +0100

    Tidied marker and slider js files

--- public/js/marker.js
+++ public/js/marker.js
@@ -1,84 +1,84 @@
 var Pear = Pear || {};
 
 Pear.getMarkerScore = function(types, price, rating) {
-    var score = 0;
+  var score = 0;
 
-    switch (types[0]) {
-        case "art_gallery":
-            if (rating && rating >= 2.5) {
-                score = 5;
-            } else {
-                score = 4;
-            }
-            break;
-        case "bar":
-            if (price) {
-                score = (price + 1);
-            } else if (rating && rating >= 2.5) {
-                score = 4;
-            } else {
-                score = 3;
-            }
-            break;
-        case "bowling_alley":
-            score = 3;
-            break;
-        case "cafe":
-            if (rating && rating >= 3.3) {
-                score = 4;
-            } else if (rating && rating <= 1.6) {
-                score = 2;
-            } else {
-                score = 3;
-            }
-            break;
-        case "casino":
-            if (rating && rating >= 2.5) {
-                score = 4;
-            } else {
-                score = 3;
-            }
-            break;
-        case "movie_theater":
-            score = 3;
-            break;
-        case "museum":
-            if (rating && rating >= 2.5) {
-                score = 5;
-            } else {
-                score = 4;
-            }
-            break;
-        case "night_club":
-            if (price && price === 4) {
-                score = 4;
-            } else if (rating && rating >= 2.5) {
-                score = 4;
-            } else {
-                score = 2;
-            }
-            break;
-        case "park":
-            score = 1;
-            break;
-        case "parking":
-            score = 1;
-            break;
-        case "restaurant":
-            if (price) {
-                score = (price + 1);
-            } else if (rating && rating <= 1.25) {
-                score = 2;
-            } else if (rating && rating <= 2.5) {
-                score = 3;
-            } else if (rating && rating <= 3.75) {
-                score = 4;
-            } else {
-                score = 5;
-            }
-            break;
+  switch (types[0]) {
+    case "art_gallery":
+    if (rating && rating >= 2.5) {
+      score = 5;
+    } else {
+      score = 4;
     }
-    return score;
+    break;
+    case "bar":
+    if (price) {
+      score = (price + 1);
+    } else if (rating && rating >= 2.5) {
+      score = 4;
+    } else {
+      score = 3;
+    }
+    break;
+    case "bowling_alley":
+    score = 3;
+    break;
+    case "cafe":
+    if (rating && rating >= 3.3) {
+      score = 4;
+    } else if (rating && rating <= 1.6) {
+      score = 2;
+    } else {
+      score = 3;
+    }
+    break;
+    case "casino":
+    if (rating && rating >= 2.5) {
+      score = 4;
+    } else {
+      score = 3;
+    }
+    break;
+    case "movie_theater":
+    score = 3;
+    break;
+    case "museum":
+    if (rating && rating >= 2.5) {
+      score = 5;
+    } else {
+      score = 4;
+    }
+    break;
+    case "night_club":
+    if (price && price === 4) {
+      score = 4;
+    } else if (rating && rating >= 2.5) {
+      score = 4;
+    } else {
+      score = 2;
+    }
+    break;
+    case "park":
+    score = 1;
+    break;
+    case "parking":
+    score = 1;
+    break;
+    case "restaurant":
+    if (price) {
+      score = (price + 1);
+    } else if (rating && rating <= 1.25) {
+      score = 2;
+    } else if (rating && rating <= 2.5) {
+      score = 3;
+    } else if (rating && rating <= 3.75) {
+      score = 4;
+    } else {
+      score = 5;
+    }
+    break;
+  }
+  return score;
 }
 
 Pear.createMarkerForClinic = function(clinic, timeout) {
@@ -131,24 +131,20 @@ Pear.createMarkerForVenue = function(venue, timeout) {
   self.addInfoWindowForVenue(venue, marker);
 }
 
-// Sets the map on all markers in the array.
 Pear.setMapOnAll = function(map) {
     for (var i = 0; i < Pear.markers.length; i++) {
         Pear.markers[i].setMap(map);
     }
 }
 
-// Shows any markers currently in the array.
 Pear.showMarkers = function() {
     Pear.setMapOnAll(Pear.map);
 }
 
-// Removes the markers from the map, but keeps them in the array.
 Pear.clearMarkers = function() {
     Pear.setMapOnAll(null);
 }
 
-// Deletes all markers in the array by removing references to them.
 Pear.deleteMarkers = function() {
     Pear.clearMarkers();
     Pear.markers = [];
@@ -161,9 +157,7 @@ Pear.loopThroughClinics = function(data) {
 }
 
 Pear.loopThroughVenues = function(data) {
-    // Pear.deleteMarkers();
     return $.each(data.results, function(i, venue) {
-
         Pear.createMarkerForVenue(venue, i * 10);
     });
 }
@@ -175,10 +169,8 @@ Pear.getClinics = function(lat, lng) {
       return $.ajax({
           type: "GET",
           url: "https://data.gov.uk/data/api/service/health/clinics?city=London"
-          // &latitude=+ lat +&longitude=+ lng;
       }).done(self.loopThroughClinics)
   })
-
 }
 
 Pear.getVenues = function(lat, lng) {
